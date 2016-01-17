682a06b031f82e4d181d9a7b7270fd55b26d9edf
dianacamacho <dcam1901@yahoo.com>
2016-01-17 16:53:40 -0600

    restful routing additions index/show/new/create/edit/update/destroy

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,27 @@
 Rails.application.routes.draw do
 
-  get '/macbookpro' => 'products#macbook'
-  get '/all' => 'products#products'
+  get '/macbook' => 'products#macbook'
+  get '/ipad' => 'products#ipad'
+  get '/appletv' => 'products#apple_tv'
+  get '/westwing' => 'products#west_wing'
+  get '/studio60' => 'products#studio_60'
+  get '/newsroom' => 'products#newsroom'
+  get '/sportsnight' => 'products#sports_night'
+  get '/wwbook' => 'products#ww_book'
+  get '/afewgoodmen' => 'products#a_few_good_men'
+  get '/jersey' => 'products#rizzo_jersey'
+  get '/' => 'products#products'
   
+  get '/products' => 'products#index'
+
+  get '/products/new' => 'products#new'
+  post 'products' => 'products#create'
+
+  get '/products/:id' => 'products#show'
+
+  get 'products/:id/edit' => 'products#edit'
+  patch 'products/:id' => 'products#update'
+  
+  delete 'products/:id' => 'products#destroy' 
+    
 end
