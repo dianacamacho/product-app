85c457dd9536771bebd952d56d2dcccd8236a282
dianacamacho <dcam1901@yahoo.com>
2016-01-19 03:12:20 -0600

    redo of all pages deleted individuals, now using bootstrap for most parts

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,6 @@
 Rails.application.routes.draw do
 
   get '/macbook' => 'products#macbook'
-  get '/ipad' => 'products#ipad'
-  get '/appletv' => 'products#apple_tv'
-  get '/westwing' => 'products#west_wing'
-  get '/studio60' => 'products#studio_60'
-  get '/newsroom' => 'products#newsroom'
-  get '/sportsnight' => 'products#sports_night'
-  get '/wwbook' => 'products#ww_book'
-  get '/afewgoodmen' => 'products#a_few_good_men'
-  get '/jersey' => 'products#rizzo_jersey'
   get '/' => 'products#products'
   
   get '/products' => 'products#index'
